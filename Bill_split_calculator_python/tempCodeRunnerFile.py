ced1b3c50d534bbe855458aa720b69a20c921735
Anushika Singh <anushikasingh7665@gmail.com>
2026-03-29 03:26:58 +0530

    Delete Bill_split_calculator_python/tempCodeRunnerFile.py

--- Bill_split_calculator_python/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-Total_bill_amount=int(input("Total bill: "))
-numbers_of_friends=int(input("Friends: "))
-bill_per_person=Total_bill_amount/numbers_of_friends
-print("Each will pay: ", bill_per_person)
